6ac873361d94d3a28f37353b6aa31b835b28eb19
Stefan Junker <code@stefanjunker.de>
2015-03-03 12:17:15 +0100

    doc generator

--- mkdocs.py
+++ mkdocs.py
@@ -4,8 +4,10 @@
 import os
 import glob
 basepath = os.path.abspath(os.getcwd())
-projecttitle = 'embEDUx'
-pages = []
+basepath = os.path.join(basepath, 'src')
+print(basepath)
+projecttile = 'embEDUx'
+pages = {}
 
 for dirpath, dirnames, filenames in os.walk(basepath):
     to_delete = []
@@ -30,29 +32,37 @@ for dirpath, dirnames, filenames in os.walk(basepath):
                 title1 = os.path.basename(f).replace('.md','').replace('-', ' ').title()
             else:
                 filename = '{}/{}'.format(reldirpath, f)
-                title1 = reldirpath.title().title()
-                title2 = os.path.basename(f).replace('.md','').replace('-', ' ').title()
+                title1 = filename.split('/',1)[0].title()
+                title2 = filename.split('/',1)[1].replace('.md','').replace('-', ' ').title()
             entry = [filename, title1, title2] 
+            if title1 not in pages:
+                pages[title1] = []
             if title2 == None:
                 del entry[2]
-                if entry[0] == ('home.md'):
-                    pages.insert(0, entry)
-                else:
-                    pages.insert(2, entry)
+#                entry[2] = 'Overview'
+                pages[title1].insert(0, entry)
             elif title1 == title2:
                 entry[2] = 'Overview'
-                pages.insert(1, entry)
+                pages[title1].insert(0, entry)
             else:
-                pages.append(entry)
+                pages[title1].append(entry)
+            print('Adding {}'.format(entry))
 
 with open('mkdocs.yml', 'w') as target, \
      open('mkdocs_head.yml','r') as head:
     for line in head.readlines():
         target.write(line)
 
-    for entry in pages:
+    def write_line(line):
         line = '- {}\n'.format(entry)
         target.write(line)
+
+    write_line(pages['Index'][0])
+    del(pages['Index'])
+
+    for category, pagelist in pages.items():
+        for entry in pagelist:
+            write_line(entry)
 
 import subprocess
 output = subprocess.check_output('mkdocs build --clean', shell=True)
